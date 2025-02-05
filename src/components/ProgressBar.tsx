e7b5202019583118cbe69d27b1c07d7085f8693e
손지원 <jiwon010206@gmail.com>
2025-02-06 01:37:35 +0900

    feat: #21 progress-bar 이동 버튼 생성

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -7,6 +7,12 @@ export default function ProgressBar() {
     <div className="w-full flex flex-col items-center">
       {/* Progress Bar */}
       <div className="relative w-full h-2 bg-gray2 rounded-full overflow-hidden my-4"></div>
+
+      {/* 버튼 */}
+      <div className="flex gap-4">
+        <button>{'<'}</button>
+        <button>다음</button>
+      </div>
     </div>
   );
 }
