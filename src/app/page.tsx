ccdd19b5646a0c616e7d15661673b180b3306762
손지원 <jiwon010206@gmail.com>
2025-02-08 01:05:00 +0900

    fix: #35 component 프로필카드 svg 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import ChatRoomHeader from '@/components/header/ChatRoomHeader';
-
 export default function Home() {
-  return <ChatRoomHeader name="김이름" profileImage="/images/profile.png" />;
+  return <div>Hello World</div>;
 }
