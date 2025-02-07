b70954f5b2da891f672e0d2859c6caa5bca7c740
손지원 <jiwon010206@gmail.com>
2025-02-07 16:09:15 +0900

    feat: #27 profileCard 프로필인증마크 생성

--- src/components/header/ProfileCard/index.tsx
+++ src/components/header/ProfileCard/index.tsx
@@ -1,9 +1,17 @@
+import Image from 'next/image';
+
 export default function ProfileCard() {
   return (
     <div>
       <div className="flex items-center space-x-2">
         <span className="text-20px font-semiBold">김이름이름</span>
         <span className="text-20px font-semiBold">20</span>
+        <Image
+          src="/images/face-auth.png"
+          alt="Face Auth Certification"
+          width={16}
+          height={16}
+        />
       </div>
     </div>
   );
