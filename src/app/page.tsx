28957396f6b3e48db3a866f14d952afea6d29480
손지원 <jiwon010206@gmail.com>
2025-02-08 21:07:33 +0900

    refactor: 프로필 이미지 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import NotificationCard from '@/components/NotificationCard';
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <NotificationCard
+      profileImg="/images/profile.png"
+      name="김이름"
+      age={20}
+      location="서울시 용산구"
+      time="PM 9:30"
+    />
+  );
 }
