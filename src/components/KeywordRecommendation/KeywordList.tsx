d8b1b8b9a0e15efac3912dd231173ba27665299b
손지원 <jiwon010206@gmail.com>
2025-02-11 01:14:52 +0900

    fix: #53 버튼 컴포넌트 사이즈 추가

--- src/components/KeywordRecommendation/KeywordList.tsx
+++ src/components/KeywordRecommendation/KeywordList.tsx
@@ -11,11 +11,11 @@ export default function KeywordList({ keywords }: KeywordListProps) {
         <div key={index} className="flex items-center justify-between pb-1">
           {/* 키워드 추천 리스트 */}
           <div className="flex items-center space-x-3">
-            <span className="text-2xl">{item.icon}</span>
-            <span className="text-gray-800">{item.title}</span>
+            <span>{item.icon}</span>
+            <span>{item.title}</span>
           </div>
           {/* 버튼 */}
-          <Button size="small" variant="filled">
+          <Button shape="circle" variant="filled" width={80} height={30}>
             선택하기
           </Button>
         </div>
