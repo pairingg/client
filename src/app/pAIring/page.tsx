ffece59001c501f6d22473862d60c4c79999383a
손지원 <jiwon010206@gmail.com>
2025-02-11 17:49:21 +0900

    feat: 메인페이지 구성 완료

--- src/app/pAIring/page.tsx
+++ src/app/pAIring/page.tsx
@@ -22,39 +22,37 @@ const profileCardList = [
 
 export default function MainPage() {
   return (
-    <div className="relative min-h-screen">
-      <main className="layout flex flex-col overflow-hidden shadow-lg pb-20">
-        <div className="w-full mx-auto px-[100px]">
-          {/* 로고 영역 */}
-          <div className="flex justify-start py-4">
-            <Link href="/pAIring">
-              <LogoIcon />
-            </Link>
-          </div>
-
-          {/* 프로필카드 영역 */}
-          <div className="flex flex-col items-center space-y-2">
-            {profileCardList.map((item, index) => (
-              <ProfileCard
-                key={index}
-                name={item.name}
-                age={item.age}
-                location={item.location}
-              />
-            ))}
-          </div>
-
-          {/* 추천 키워드 영역 */}
-          <div className="flex flex-col mt-6 pb-6">
-            <p className="font-24-bold pt-6 pb-4">맞춤 추천</p>
-            <div className="items-center">
-              <KeywordRecommendation keywords={keywords} />
-            </div>
-          </div>
+    <div className="relative min-h-screen pb-20">
+      <div className="w-full mx-auto px-[100px]">
+        {/* 로고 영역 */}
+        <div className="flex justify-start py-6 -ml-6">
+          <Link href="/pAIring">
+            <LogoIcon />
+          </Link>
         </div>
 
-        <BottomNavBar />
-      </main>
+        {/* 프로필카드 영역 */}
+        <div className="flex flex-col items-center space-y-2">
+          {profileCardList.map((item, index) => (
+            <ProfileCard
+              key={index}
+              name={item.name}
+              age={item.age}
+              location={item.location}
+            />
+          ))}
+        </div>
+
+        {/* 추천 키워드 영역 */}
+        <div className="flex flex-col mt-6 pb-6">
+          <p className="font-24-bold pt-6 pb-4 -ml-6">맞춤 추천</p>
+          <div className="flex justify-center">
+            <KeywordRecommendation keywords={keywords} />
+          </div>
+        </div>
+      </div>
+
+      <BottomNavBar />
     </div>
   );
 }
