3b0f5111e4ca29e385014c7447a12c2b52701d15
y00eunji <aine0227@naver.com>
2025-02-12 09:08:24 +0900

    feat: #56 타이틀 컴포넌트 추가

---
+++ src/components/onboarding/Title/index.tsx
@@ -0,0 +1,20 @@
+interface TitleProps {
+  title: string;
+  currentStepNumber?: number;
+  totalStepsNumber?: number;
+}
+
+export default function Title({
+  title,
+  currentStepNumber,
+  totalStepsNumber,
+}: TitleProps) {
+  return (
+    <div className="flex items-center gap-2">
+      <h2 className="font-24-bold">{title}</h2>
+      <p className="font-14-regular font-roboto text-gray1 text-center">
+        {currentStepNumber} / {totalStepsNumber}
+      </p>
+    </div>
+  );
+}
