104f1bf349bbe5d1b16dfad9bd4d3b0fff0e45c9
y00eunji <aine0227@naver.com>
2025-02-15 15:51:50 +0900

    feat: #65 프로필 등록 완성 페이지 수정

--- src/components/onboarding/steps/profile/Complete.tsx
+++ src/components/onboarding/steps/profile/Complete.tsx
@@ -14,29 +14,27 @@ export default function Complete() {
 
   return (
     <div className="relative h-[100dvh]">
-      <div className="w-full mt-14">
+      <div className="w-full pt-14">
         <ProgressBar currentStep={9} totalSteps={8} />
       </div>
       <div className="w-full px-5 py-8 flex flex-col">
-        <div>
-          <div className="mb-10">
-            <Title title="프로필 등록이 완료되었어요." />
-            <div className="font-14-regular mt-[10px]">
-              이후 이상형 정보 등록이 시작됩니다.
-            </div>
+        <div className="mb-10">
+          <Title title="프로필 등록이 완료되었어요." />
+          <div className="font-14-regular mt-[10px]">
+            이후 이상형 정보 등록이 시작됩니다.
           </div>
         </div>
+      </div>
 
-        <div className="absolute bottom-0 left-0 w-full px-5 py-8">
-          <Button
-            shape="rectangle"
-            variant="filled"
-            className="w-full h-[55px]"
-            onClick={handleNext}
-          >
-            다음
-          </Button>
-        </div>
+      <div className="absolute bottom-0 left-0 w-full px-5 py-8">
+        <Button
+          shape="rectangle"
+          variant="filled"
+          className="w-full h-[55px]"
+          onClick={handleNext}
+        >
+          다음
+        </Button>
       </div>
     </div>
   );
