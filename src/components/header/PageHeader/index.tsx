1f2df8fd153ad7668b28ecaecd371bed41398cf0
손지원 <jiwon010206@gmail.com>
2025-02-16 18:49:06 +0900

    feat: #71 알림카드 컴포넌트 조건처리 및 페이지 적용

--- src/components/header/PageHeader/index.tsx
+++ src/components/header/PageHeader/index.tsx
@@ -4,7 +4,7 @@ interface PageHeaderProps {
 
 export default function PageHeader({ title }: PageHeaderProps) {
   return (
-    <div className="px-7 pt-3">
+    <div className="px-7 pt-3 pb-3">
       <p className="text-24px font-bold">{title}</p>
     </div>
   );
