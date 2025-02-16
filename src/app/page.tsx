5bf507275e30c681d798374ec4f683f0a04748c8
y00eunji <aine0227@naver.com>
2025-02-17 08:23:31 +0900

    feat: #74 SEO 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Script from 'next/script';
+
 export default function Home() {
-  return <div className="w-full h-screen"></div>;
+  return (
+    <div>
+      <Script
+        defer
+        src="https://cdn.swygbro.com/public/widget/swyg-widget.js"
+      ></Script>
+    </div>
+  );
 }
