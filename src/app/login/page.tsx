24e15c4dccdb64a808e1d969094905d2124e9c8d
y00eunji <aine0227@naver.com>
2025-02-25 08:44:08 +0900

    feat: #56 로그인 화면

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,19 @@
-export default function Login() {
-  return <div>Login</div>;
+import Login from '@/components/login';
+
+export default function LoginPage({
+  searchParams,
+}: {
+  searchParams: { code?: string };
+}) {
+  const { code } = searchParams;
+
+  if (code) {
+    console.log('Authorization code:', code);
+  }
+
+  return (
+    <div className="h-full px-5 py-10">
+      <Login code={code} />
+    </div>
+  );
 }
