252f6e869f656772695c966bb9c078c8b24500fa
손지원 <jiwon010206@gmail.com>
2025-02-26 18:41:02 +0900

    feat: 온보딩 여부 확인

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,20 @@ import { useEffect, useState } from 'react';
 import MainOnboarding from '@/components/MainOnboarding';
 import Splash from '@/components/Splash';
 
+import Login from './login/page';
+
 export default function HomePage() {
   const [showSplash, setShowSplash] = useState(true);
+  const [hasSeenOnboarding, setHasSeenOnboarding] = useState(false);
 
   useEffect(() => {
+    // localStorage에서 온보딩 여부 확인
+    const seen = localStorage.getItem('hasSeenOnboarding');
+    if (seen) {
+      setHasSeenOnboarding(true);
+    }
+
+    // 스플래시 화면은 2초
     const timer = setTimeout(() => {
       setShowSplash(false);
     }, 2000);
@@ -16,5 +26,16 @@ export default function HomePage() {
     return () => clearTimeout(timer);
   }, []);
 
-  return <>{showSplash ? <Splash /> : <MainOnboarding />}</>;
+  return (
+    <>
+      {showSplash ? (
+        <Splash />
+      ) : // 온보딩을 이미 본 사용자는 로그인 화면, 그렇지 않으면 메인 온보딩 화면
+      hasSeenOnboarding ? (
+        <Login />
+      ) : (
+        <MainOnboarding />
+      )}
+    </>
+  );
 }
