5b39117bed32a025df83fd61be6aa901a02fa412
손지원 <jiwon010206@gmail.com>
2025-03-17 22:53:13 +0900

    chore: meta 태그 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
         <meta property="og:url" content="웹 페이지 주소 들어갈 예정" />
         <meta property="og:image" content="/public/images/og-image.png" />
         <meta
-          http-equiv="Content-Security-Policy"
+          httpEquiv="Content-Security-Policy"
           content="upgrade-insecure-requests"
         />
         <title>pAIring</title>
